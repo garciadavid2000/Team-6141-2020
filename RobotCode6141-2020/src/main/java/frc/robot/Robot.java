9743f59263408498b760d29a20583deb7d85dbca
XbamcX <60197781+XbamcX@users.noreply.github.com>
2020-02-18 16:46:16 -0500

    commit to 'Brendan'

--- RobotCode6141-2020/src/main/java/frc/robot/Robot.java
+++ RobotCode6141-2020/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.ControlType;
+import com.revrobotics.EncoderType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.analog.adis16470.frc.ADIS16470_IMU;
 
@@ -60,10 +61,10 @@ public class Robot extends TimedRobot {
   // intake
   // private Spark intake = new Spark(6);
 
-  private VictorSP leftMotor1 = new VictorSP(1);
+  private CANSparkMax leftMotor1 = new CANSparkMax(1, MotorType.kBrushless);
   private WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(1);
   private WPI_VictorSPX leftMotor3 = new WPI_VictorSPX(2);
-  private VictorSP rightMotor1 = new VictorSP(2);
+  private CANSparkMax rightMotor1 = new CANSparkMax(1, MotorType.kBrushless);
   private WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(3);
   private WPI_VictorSPX rightMotor3 = new WPI_VictorSPX(4);
 
@@ -72,26 +73,13 @@ public class Robot extends TimedRobot {
   private DifferentialDrive driveTrain = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
 
   // shooter
-  private CANSparkMax shooter1 = new CANSparkMax(9, MotorType.kBrushless);
-  private CANSparkMax shooter2 = new CANSparkMax(8, MotorType.kBrushless);
+  private VictorSP shooter1 = new VictorSP(9);
+  private VictorSP shooter2 = new VictorSP(8);
   private SpeedControllerGroup shooter = new SpeedControllerGroup(shooter1, shooter2);
 
-  // Alt Encoder 1
-  private static final AlternateEncoderType kAltEncType = AlternateEncoderType.kQuadrature;
-  private static final int kCPR = 8192;
-
-  private CANPIDController m_pidController;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
-
-  private CANEncoder m_altEncoder;
-
-  // Alt Encoder 2
-  private static final AlternateEncoderType kAltEncType_2 = AlternateEncoderType.kQuadrature;
-  private static final int kCPR_2 = 8192;
-  private CANPIDController m_pidController_2;
-  public double kP_2, kI_2, kD_2, kIz_2, kFF_2, kMaxOutput_2, kMinOutput_2;
-
-  private CANEncoder m_altEncoder2;
+  //encoder
+  private CANEncoder leftEncoder = leftMotor1.getEncoder(EncoderType.kQuadrature, 4096);
+ 
 
   // limelight
 
@@ -147,61 +135,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
-    m_altEncoder = shooter1.getAlternateEncoder(kAltEncType, kCPR);
-    m_altEncoder2 = shooter2.getAlternateEncoder(kAltEncType_2, kCPR_2);
-
-    m_pidController = shooter1.getPIDController();
-    m_pidController_2 = shooter2.getPIDController();
-
-    m_pidController.setFeedbackDevice(m_altEncoder);
-    m_pidController_2.setFeedbackDevice(m_altEncoder2);
-
-    // PID Co-efficients
-    kP = kP_2 = 0.1;
-    kI = kI_2 = 1e-4;
-    kD = kD_2 = 1;
-    kIz = kIz_2 = 0;
-    kFF = kFF_2 = 0;
-    kMaxOutput = kMaxOutput_2 = 1;
-    kMinOutput = kMinOutput_2 = -1;
-
-    // Set PID Coefficients
-    m_pidController.setP(kP);
-    m_pidController_2.setP(kP_2);
-
-    m_pidController.setI(kI);
-    m_pidController_2.setI(kI_2);
-
-    m_pidController.setD(kD);
-    m_pidController_2.setD(kD_2);
-
-    m_pidController.setIZone(kIz);
-    m_pidController_2.setIZone(kIz_2);
-
-    m_pidController.setFF(kFF);
-    m_pidController_2.setFF(kFF_2);
-
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-    m_pidController_2.setOutputRange(kMinOutput_2, kMaxOutput_2);
-
-    // Display PID coefficients to SmartDashboard
-    SmartDashboard.putNumber("P Gain", kP);
-    SmartDashboard.putNumber("I Gain", kI);
-    SmartDashboard.putNumber("D Gain", kD);
-    SmartDashboard.putNumber("I Zone", kIz);
-    SmartDashboard.putNumber("Feed Forward", kFF);
-    SmartDashboard.putNumber("Max Output", kMaxOutput);
-    SmartDashboard.putNumber("Min Output", kMinOutput);
-    SmartDashboard.putNumber("Set Rotations", 0);
-
-    SmartDashboard.putNumber("P Gain 2", kP_2);
-    SmartDashboard.putNumber("I Gain 2", kI_2);
-    SmartDashboard.putNumber("D Gain 2", kD_2);
-    SmartDashboard.putNumber("I Zone 2", kIz_2);
-    SmartDashboard.putNumber("Feed Forward 2", kFF_2);
-    SmartDashboard.putNumber("Max Output 2", kMaxOutput_2);
-    SmartDashboard.putNumber("Min Output 2", kMinOutput_2);
-    SmartDashboard.putNumber("Set Rotations 2", 0);
+   
 
     chooser.setDefaultOption("Default Auto", kDefaultAuto);
     chooser.addOption("My Auto", kCustomAuto);
@@ -273,84 +207,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    // PID stuff
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
-    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
-
-    double p2 = SmartDashboard.getNumber("P Gain 2", 0);
-    double i2 = SmartDashboard.getNumber("I Gain 2", 0);
-    double d2 = SmartDashboard.getNumber("D Gain 2", 0);
-    double iz2 = SmartDashboard.getNumber("I Zone 2", 0);
-    double ff2 = SmartDashboard.getNumber("Feed Forward 2", 0);
-    double max2 = SmartDashboard.getNumber("Max Output 2", 0);
-    double min2 = SmartDashboard.getNumber("Min Output 2", 0);
-    double rotations2 = SmartDashboard.getNumber("Set Rotations 2", 0);
-
-    // if PID coefficients have changed write new values to controller
-    if ((p != kP)) {
-      m_pidController.setP(p);
-      kP = p;
-    }
-    if ((i != kI)) {
-      m_pidController.setI(i);
-      kI = i;
-    }
-    if ((d != kD)) {
-      m_pidController.setD(d);
-      kD = d;
-    }
-    if ((iz != kIz)) {
-      m_pidController.setIZone(iz);
-      kIz = iz;
-    }
-    if ((ff != kFF)) {
-      m_pidController.setFF(ff);
-      kFF = ff;
-    }
-    if ((max != kMaxOutput) || (min != kMinOutput)) {
-      m_pidController.setOutputRange(min, max);
-      kMinOutput = min;
-      kMaxOutput = max;
-
-    }
-    if ((p2 != kP_2)) {
-      m_pidController_2.setP(p2);
-      kP_2 = p2;
-    }
-    if ((i2 != kI_2)) {
-      m_pidController_2.setI(i2);
-      kI_2 = i2;
-    }
-    if ((d2 != kD_2)) {
-      m_pidController_2.setD(d2);
-      kD_2 = d2;
-    }
-    if ((iz2 != kIz_2)) {
-      m_pidController_2.setIZone(iz2);
-      kIz_2 = iz2;
-    }
-    if ((ff2 != kFF_2)) {
-      m_pidController_2.setFF(ff2);
-      kFF_2 = ff2;
-    }
-    if ((max2 != kMaxOutput_2) || (min2 != kMinOutput_2)) {
-      m_pidController_2.setOutputRange(min2, max2);
-      kMinOutput_2 = min2;
-      kMaxOutput_2 = max2;
-    }
-
-    m_pidController.setReference(rotations, ControlType.kPosition);
-    m_pidController_2.setReference(rotations2, ControlType.kPosition);
-
-    SmartDashboard.putNumber("SetPoint", rotations);
-    SmartDashboard.putNumber("ProcessVariables", m_altEncoder.getPosition());
+    
+   
 
     
     
